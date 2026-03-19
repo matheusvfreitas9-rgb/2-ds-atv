c1ac533e233f646d838e759cfabac83671a5d822
matheus <matheus.v.freitas9@aluno.senai.br>
2026-03-19 11:43:44 -0300

    arreis bidimensionais

---
+++ atividade_3/cinema.js
@@ -0,0 +1,10 @@
+let assentos = [
+    ["L", "L", "O" ],
+    ["L", "O", "L"],
+    ["O", "L", "L"]
+];
+
+let cadeiraMeio = assentos[1][1];
+console.log("A cadeira do meio está" + cadeiraMeio);
+assentos[0][0] = "O";
+console.log("O assento em [0][0] esstá" + assentos[0][0])
